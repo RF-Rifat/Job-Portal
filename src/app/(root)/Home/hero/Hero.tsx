afc9e317711f186f36daa4bfdcc0e4b453a26e1a
Shakil Ahmmed <shakilahmmed8882@gmail.com>
2024-11-29 22:38:17 +0700

    test

--- src/app/(root)/Home/hero/Hero.tsx
+++ src/app/(root)/Home/hero/Hero.tsx
@@ -9,6 +9,7 @@ import { SearchBar } from "./components/SearchBar";
 import Stats from "./components/Stats";
 import FloatingIcons from "./components/FloatingIcons";
 import FadeUp from "@/shared/animation/FadeUp";
+import Heading from "./components/Heading";
 
 const suggestions = [
   "Designer",
@@ -35,7 +36,7 @@ const Hero = () => {
     <section className="relative min-h-[80vh] bg-white flex items-center justify-center py-11">
       {/* Background Image */}
       <div className="absolute inset-0">
-        <img
+        <motion.img
           src="/home/heroBg.svg"
           alt="Hero Background"
           className="w-full h-full object-top object-cover"
@@ -76,22 +77,8 @@ const Hero = () => {
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
           >
-            {/* Headline */}
-            <div className="space-y-4">
-              <motion.h1
-                className="text-4xl sm:text-5xl mt-8 md:text-5xl font-bold tracking-tight"
-                initial={{ opacity: 0, y: 20 }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{ duration: 0.5, delay: 0.2 }}
-              >
-                Find Your Perfect
-                <span className="text-primary"> Career Path</span>
-              </motion.h1>
-              <FadeUp>
-              <p className="text-lg">Discover opportunities that match your skills and interests. Whether
-              you're looking for a full-time job or freelance gigs.</p>
-              </FadeUp>
-            </div>
+            
+            <Heading/>
 
             {/* Search Bars */}
             <div className="space-y-4 w-full">
