a7cb9f1a7c644ad98a5814c3de98b847368b7dbd
Rf-Rifat <rfrifat6344@gmail.com>
2024-11-30 00:09:53 +0600

    feat:home tophiring customer review newslette completed

--- src/components/ui/BackgroundPatterns.tsx
+++ src/components/ui/BackgroundPatterns.tsx
@@ -11,7 +11,9 @@ export default function BackgroundPatterns() {
       <div
         className="absolute -left-[10%] -bottom-[10%] h-[500px] w-[500px] rounded-full bg-secondary/10 blur-3xl"
         aria-hidden="true"
-      />
+          />
+          
+          
     </div>
   );
 }
