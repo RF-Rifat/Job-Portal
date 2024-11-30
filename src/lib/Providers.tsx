37c0a3e8f19cbc0c73dd5ef90742f54a923687bd
Rf-Rifat <rfrifat6344@gmail.com>
2024-11-30 16:28:33 +0600

    feat:theme removed

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -2,22 +2,10 @@
 
 import React from "react";
 import { Provider } from "react-redux";
-import { ThemeProvider } from "next-themes";
 import { store } from "@/redux/store/store";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <Provider store={store}>
-      <ThemeProvider
-        attribute="class"
-        defaultTheme="system"
-        enableSystem
-        disableTransitionOnChange
-      >
-        {children}
-      </ThemeProvider>
-    </Provider>
-  );
+  return <Provider store={store}>{children}</Provider>;
 };
 
 export default Providers;
